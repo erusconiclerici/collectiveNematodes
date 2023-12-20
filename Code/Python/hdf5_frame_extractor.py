bf1b50ce3dbc734eb225f4e549ec842084baae9c
erusconiclerici <erusconi@ab.mpg.de>
2023-12-20 19:11:04 +0100

    This is the new structure of the extractedFrame /Volumes/TOSHIBA_EXT/Phenotype_features_collective/ExtractedFrames/     ├─ Strain1/     │   ├─ ExperimentName1/     │   │   ├─ 000001.jpg     │   │   ├─ 000002.jpg     │   │   ├─ 000003.jpg     │   │   └─ ...     │   ├─ ExperimentName2/     │   │   ├─ 000001.jpg     │   │   ├─ 000002.jpg     │   │   └─ ...     │   └─ ...     ├─ Strain2/     │   ├─ ExperimentName3/     │   │   ├─ 000001.jpg     │   │   ├─ 000002.jpg     │   │   └─ ...     │   └─ ...     └─ ...

--- Code/Python/hdf5_frame_extractor.py
+++ Code/Python/hdf5_frame_extractor.py
@@ -8,13 +8,11 @@ def ensure_directory_exists(path):
         os.makedirs(path)
 
 def extract_experiment_name(file_path):
-    # Assuming the experiment's name is part of the file's name
     base_name = os.path.basename(file_path)
-    name_parts = base_name.split('_')
-    experiment_name = '_'.join(name_parts[:-1])  # Exclude the extension and any other parts
+    experiment_name = os.path.splitext(base_name)[0]  # Removes the file extension
     return experiment_name
 
-def create_output_directories_for_experiments(root_path):
+def create_output_directories_for_experiments(root_path, common_folder='ExtractedFrames'):
     output_dirs = {}
     strain_folders = [d for d in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, d))]
     
@@ -22,7 +20,7 @@ def create_output_directories_for_experiments(root_path):
         hdf5_files = glob.glob(os.path.join(root_path, strain, '*.hdf5'))
         for file_path in hdf5_files:
             experiment_name = extract_experiment_name(file_path)
-            output_dir = os.path.join(root_path, strain, experiment_name)
+            output_dir = os.path.join(root_path, common_folder, strain, experiment_name)
             ensure_directory_exists(output_dir)
             output_dirs[file_path] = output_dir
 
@@ -38,7 +36,7 @@ def extract_frames_from_hdf5(file_path, output_directory, step=500):
 # Set the root path where the HDF5 files are stored
 root_path = '/Volumes/TOSHIBA_EXT/Phenotype_features_collective/Data'
 
-# Create output directories for each experiment under each strain
+# Create output directories for each experiment under each strain in the common folder 'ExtractedFrames'
 output_directories = create_output_directories_for_experiments(root_path)
 
 # Extract frames from HDF5 files
